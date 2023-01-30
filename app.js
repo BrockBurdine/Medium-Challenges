c88d875297d290e37b9b02dc60007bd341c08b16
Brock <brockburdine@gmail.com>
2023-01-30 11:33:37 -0600

    reverseString

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-function getMax(arr) {
-  let max = arr[1];
-  for (let i = 0; i <= arr.length; ++i) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
+function reverseString(str) {
+  let reverseString = '';
+  for (let i = 0; i < str.length; ++i) {
+    // This is how you loop through every character in a string
+    console.log(str[i]);
+    reverseString = str[i] + reverseString;
   }
-  return max;
+  return reverseString;
 }
-
-console.log(getMax([-100, -200, -300]));
+console.log(reverseString("abc"));
+// turning abc into "cba"
